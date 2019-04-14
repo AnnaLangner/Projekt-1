201ec3d75e895d47a2e146d9eaef0e3441ffb143
Ania <annalangner06@gmail.com>
2019-04-14 14:51:26 +0200

    Adding a chart

--- js/script.js
+++ js/script.js
@@ -65,3 +65,52 @@ var showQuitModal = function openModal(modal) {
 
 btnQuitModal.addEventListener('click', showQuitModal);
 
+var ctx = document.getElementById('myChart').getContext('2d');
+
+var chart = new Chart(ctx, {
+    // 1
+    type: 'bar',
+    data: {
+        // 2
+        labels: ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10"],
+        // 3
+        datasets: [{
+            // 4
+            label: "Signups",
+            // 5
+            backgroundColor: '#8DBEC8',
+            borderColor: '#8DBEC8',
+            // 6
+            data: [ 52, 51, 41, 94, 26, 6, 72, 9, 21, 88 ],
+        },
+        {
+            label: "FTD",
+            backgroundColor: '#F29E4E',
+            borderColor: '#F29E4E',
+            data: [ 6, 72, 1, 0, 47, 11, 50, 44, 63, 76 ],
+        },
+        {
+            label: "Earned",
+            backgroundColor: '#71B374',
+            borderColor: '#71B374',
+            data: [ 59, 49, 68, 90, 67, 41, 13, 38, 48, 48 ],
+            // 7
+            hidden: true,
+        }]
+    },
+    options: {
+        title: {
+            display: true,
+            text: 'Earnings chart',
+            position: 'top',
+            fontSize: 20
+        }
+        // legend: {
+        //     display: true,
+        //     labels: {
+        //         fontSize: 20
+        //     }
+        // }
+    }
+});
+
